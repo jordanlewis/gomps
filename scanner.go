e7ecfb654dae334d67b1aa345d08dff02ea15c00
Jordan Lewis <jalewis@uchicago.edu>
2009-11-30 06:35:44 -0600

    Complete simple scanner

--- scanner.go
+++ scanner.go
@@ -1,14 +1,24 @@
-package scanner
+package gomps
 
+/*
 import (
 	"bytes";
-	"./token";
 	"strconv";
 )
+*/
+import (
+	"fmt";
+	"io";
+)
+
+func reportError(err string, pos Position) {
+	fmt.Printf("Error in scanning: %s.%s:%s): %s\n", pos.Filename,
+			   pos.Line, pos.Column, err);
+}
 
 func isLetter(char int) bool {
 	return	('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') ||
-			(char == '_')
+			(char == '_') || (char == '.')
 }
 
 func isDigit(char int) bool {
@@ -18,23 +28,79 @@ func isDigit(char int) bool {
 type Scanner struct {
 	input	[]byte;
 
-	pos		token.Position;
+	pos		Position;
 	offset	int;
 	c	int;
 }
 
 func (S *Scanner) Init(filename string, input []byte) {
 	S.input = input;
-	S.pos = token.Position{filename, 0, 1, 0};
+	S.pos = Position{filename, 0, 1, 0};
 	S.offset = 0;
 	S.next();
+}
+
+func (S *Scanner) scanIdentifier() Token {
+	tok := INSTR;
+	if S.c == '.' {
+		tok = DIRECTIVE;
+	}
+	for isLetter(S.c) || isDigit(S.c) {
+		S.next()
+	}
+	if S.c == ':' {
+		S.next();
+		if tok == DIRECTIVE {
+			tok = ILLEGAL;
+		} else {
+			tok = LABEL;
+		}
+	}
+	return tok;
+}
+
+func (S *Scanner) scanNumber() Token {
+	for isDigit(S.c) {
+		S.next()
+	}
+	return INT;
+}
+
+func (S *Scanner) scanEscape() {
+	S.next();
+	switch S.c {
+	case 'a', 'b', 'f', 'n', 'r', 't', 'v', '\\', '"':
+	default:
+		fmt.Printf("Illegal character escape\n");
+	}
+}
+
+func (S *Scanner) scanString() {
+	for S.c != '"' {
+		if S.c == '\n' || S.c < 0 {
+			fmt.Printf("String unterminated\n");
+			break;
+		}
+		if S.c == '\\' {
+			S.scanEscape();
+		}
+		S.next();
+	}
+
+	S.next();
+}
+
+func (S *Scanner) scanReg() {
+	for isLetter(S.c) || isDigit(S.c) {
+		S.next()
+	}
 }
 
 func (S *Scanner) next() {
 	if S.offset < len(S.input) {
 		S.pos.Offset = S.offset;
 		S.pos.Column++;
-		r := int(S.input[S.offset])
+		r := int(S.input[S.offset]);
 		if r == '\n' {
 			S.pos.Line++;
 			S.pos.Column = 0;
@@ -47,16 +113,64 @@ func (S *Scanner) next() {
 	}
 }
 
-func (S *Scanner) Scan() (pos token.Position, tok token.Token, word []byte) {
+func (S *Scanner) Scan() (pos Position, tok Token, word []byte) {
 restart_scan:
-	for S.c == ' ' || S.c == '\t' || S.c == '\n' || S.c == '\r' {
+	for S.c == ' ' || S.c == ',' || S.c == '\t' || S.c == '\n' || S.c == '\r' {
 		S.next()
 	}
-	switch c := S.c {
-	case == '#' { // Found a comment; go to next line and restart scan
-		for S.pos.Column != 0 {
-			S.next()
+
+	pos, tok = S.pos, ILLEGAL;
+
+	switch c := S.c; {
+	case isLetter(c):
+		tok = S.scanIdentifier()
+	case isDigit(c):
+		tok = S.scanNumber()
+	default:
+		S.next();
+		switch c {
+		case -1:
+			tok = EOF
+		case '"':
+			tok = STRING;
+			S.scanString();
+		case '$':
+			tok = REG;
+			S.scanReg();
+		case '(':
+			tok = LPAREN
+		case ')':
+			tok = RPAREN
+
+		case '#': // Found a comment; go to next line and restart scan
+			for S.pos.Column != 0 {
+				S.next()
+			}
+			goto restart_scan
+		default:
+			fmt.Printf("Illegal char %c\n", c);
+			tok = EOF;
 		}
-		goto restart_scan
+	}
+	return pos, tok, S.input[pos.Offset:S.pos.Offset];
+}
+
+// func Tokenize(filename string, input []byte) int {
+// 	var s Scanner;
+// 	s.Init(filename, input);
+// 	for f(s.Scan()) {
+// 		
+// 	}
+// 	return 0;
+// }
+func Tokenize(filename string) {
+	var s Scanner;
+	input, _ := io.ReadFile(filename);
+	s.Init(filename, input);
+	token := ILLEGAL;
+	for token != EOF {
+		pos, tok, word := s.Scan();
+		token = tok;
+		fmt.Printf("%s@%d(%d:%d) %s %s\n", pos.Filename, pos.Offset, pos.Line, pos.Column, tokToString(token), word);
 	}
 }
--- token.go
+++ token.go
@@ -1,9 +1,11 @@
-package token
+package gomps
 
+/*
 import (
     "fmt";
     "strconv";
 )
+*/
 
 type Token int
 
@@ -12,11 +14,16 @@ const (
 	EOF;
 
 	INT;	// 345
-	FLOAT:	// 4.34
+	FLOAT;	// 4.34
 	LABEL;	// foo:
 	STRING;	// "foo"
-	REG:	// $r3
-	FPREG:	// $f3
+	REG;	// $r3
+	FPREG;	// $f3
+	INSTR;	// abs.d
+	DIRECTIVE;	//.align
+
+	LPAREN;
+	RPAREN;
 )
 
 type Position struct {
@@ -24,4 +31,23 @@ type Position struct {
 	Offset		int;
 	Line		int;
 	Column		int;
+}
+
+func tokToString(tok Token) string{
+	var str string;
+	switch tok {
+	case ILLEGAL: str = "ILLEGAL";
+	case EOF: str = "EOF";
+	case INT: str = "INT";
+	case FLOAT: str = "FLOAT";
+	case LABEL: str = "LABEL";
+	case STRING: str = "STRING";
+	case REG: str = "REG";
+	case FPREG: str = "FPREG";
+	case INSTR: str = "INSTR";
+	case DIRECTIVE: str = "DIRECTIVE";
+	case LPAREN: str = "LPAREN";
+	case RPAREN: str = "RPAREN";
+	}
+	return str;
 }
